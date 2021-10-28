5f71a4a3fc277a89af4fd83f20c94cdb5c72fee5
Stackbit Code Editor <projects@stackbit.com>
2021-10-28 18:26:51 +0000

    LogoSection.js: changed

--- src/components/LogoSection.js
+++ src/components/LogoSection.js
@@ -2,7 +2,7 @@ const LogoSection = (props) => {
     const { logos, title } = props
     return (
         <div className="max-w-screen-xl mx-auto px-4 sm:px-6 py-14 lg:py-20 mt-10 mb-10 text-center">
-            <h1 className="text-3xl tracking-tight sm:text-4xl mb-2">{title}</h1>
+            <h1 className="text-3xl tracking-tight sm:text-4xl mb-2" data-sb-field-path=".title">{title}</h1>
             <div className="flex justify-center items-center">
                 {logos.map((logo, index) => (
                     <div className="p-6" key={index}>
