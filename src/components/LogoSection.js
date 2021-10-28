e982fa06973b2eb493dc6f0b43f94f4a370676ef
Stackbit Code Editor <projects@stackbit.com>
2021-10-28 18:29:43 +0000

    LogoSection.js: changed

--- src/components/LogoSection.js
+++ src/components/LogoSection.js
@@ -1,5 +1,5 @@
 const LogoSection = (props) => {
-    const { logos, title } = props
+    const { annotationPrefix, logos, title } = props
     return (
         <div className="max-w-screen-xl mx-auto px-4 sm:px-6 py-14 lg:py-20 mt-10 mb-10 text-center">
             <h1 className="text-3xl tracking-tight sm:text-4xl mb-2" data-sb-field-path=".title">{title}</h1>
